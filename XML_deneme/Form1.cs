9604facb6ec0d6baa9f1a9bb6332dd5f48a5e2f6
esraerik <esraerik1@gmail.com>
2014-12-04 21:16:50 +0200

    xml
    1

---
+++ XML_deneme/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+ using     System.Xml.Schema;
+   using   System.Xml.Serialization;
+ using     System.Xml.XPath ;
+using System.Xml.Xsl;
+namespace XML_deneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ XmlDocument myDoc = new XmlDocument();
+        myDoc.Load(MapPath("DTDl2.xml"));
+        XmlNodeList rankList = myDoc.SelectNodes("/fsm/states/state[@name='start']/transition/Rank");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlStateMachine fsm = new XmlStateMachine();
+            fsm.StateTable = "DTDl2.xml";
+            fsm.CurrentState = "start";
+           
+
+            XmlTextReader xmlReader = new XmlTextReader("DTDl2.xml");
+            XmlNodeType myNodeType = xmlReader.NodeType;
+            xmlReader.Read();
+            while (xmlReader.Read())
+            {
+                string value = doc.XPathSelectElement("//requisitionData[2]/element1").Value;
+             // textBox1.Text = xmlReader.GetType().ToString();
+                //xmlReader.MoveToElement();
+              /*if (xmlReader.Namespaces.ToString() == "start")
+               {
+                    //fsm.CurrentState = xmlReader.ReadString();
+                    textBox1.Text =fsm.CurrentState;
+               }*/
+               if ((myNodeType == XmlNodeType.Text))
+               {
+                  textBox1.Text=  xmlReader.Name.ToString();
+              }
+              //  fsm.CurrentState = xmlReader.ReadString();
+               // textBox1.Text = fsm.CurrentState;
+            }
+        }
+    }
+}
+   
---
+++ XML_deneme/XmlStateMachine.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Diagnostics;
+
+namespace XML_deneme
+{
+    class XmlStateMachine
+    {
+
+        private XmlTextReader tableParser;
+        private string stateCurrent;
+        private string stateTable;
+        private string action;
+        public XmlStateMachine() {
+            tableParser = null;
+            stateCurrent = String.Empty;
+			stateTable = String.Empty;
+			action = String.Empty;
+		}
+        public string CurrentState
+        {
+            get
+            {
+                return stateCurrent;
+            }
+            set
+            {
+                stateCurrent = value;
+            }
+        }
+        public string Action
+        {
+            get
+            {
+                return action;
+            }
+        }
+
+        public string StateTable
+        {
+            get
+            {
+                return stateTable;
+            }
+            set
+            {
+                stateTable = value;
+            }
+        }
+        public string Next(string inputArg)
+        {
+            string nextState = String.Empty;
+
+            if (CurrentState != String.Empty)
+            {
+                try
+                {
+                     tableParser = new XmlTextReader(StateTable);
+
+                    while (true == tableParser.Read())
+                    {
+                        if (XmlNodeType.Element == tableParser.NodeType)
+                        {
+                            if (true == tableParser.HasAttributes)
+                            {
+                                string state = tableParser.GetAttribute("name");
+                                if (state == CurrentState)
+                                {
+                                    // Get transition data
+                                    while (true == tableParser.Read())
+                                    {
+                                        if ((XmlNodeType.Element == tableParser.NodeType) && ("transition" == tableParser.Name))
+                                        {
+                                            if (true == tableParser.HasAttributes)
+                                            {
+                                                string input = tableParser.GetAttribute("input");
+                                                if (input == inputArg)
+                                                {
+                                                    CurrentState = tableParser.GetAttribute("next");
+                                                    nextState = CurrentState;
+                                                    action = tableParser.GetAttribute("action");
+                                                    return nextState;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (XmlException e)
+                {
+                    // Eliminate default trace listener
+                    Trace.Listeners.RemoveAt(0);
+                    // Add console trace listener
+                    TextWriterTraceListener myWriter = new TextWriterTraceListener(System.Console.Out);
+                    Trace.Listeners.Add(myWriter);
+                    Trace.WriteLine("[XMLStateMachine] Could not load state table definition.");
+                    Trace.Indent();
+                    Trace.WriteLine(e.Message);
+                    Trace.Unindent();
+                    // 	Clean up object
+                    tableParser.Close();
+                    tableParser = null;
+                    stateCurrent = String.Empty;
+                    action = String.Empty;
+                }
+            }
+
+            return nextState;
+        }
+
+     
+    }
+}
